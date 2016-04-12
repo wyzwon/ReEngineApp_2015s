df99e0c731ef611945a90c5724b81982cc0a7ccc
Luis Bobadilla Sotelo <labigm@ad.rit.edu>
2016-04-12 12:26:43 -0400

    in class changes

--- E12_BoundingSphereClass/AppClass.cpp
+++ E12_BoundingSphereClass/AppClass.cpp
@@ -109,17 +109,40 @@ void AppClass::Update(void)
 	m_pMeshMngr->SetModelMatrix(glm::translate(m_v3O1) * ToMatrix4(m_qArcBall), "Steve");
 	m_pMeshMngr->SetModelMatrix(glm::translate(m_v3O2), "Creeper");
 
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+
+	
+
 	//Adds all loaded instance to the render list
 	m_pMeshMngr->AddInstanceToRenderList("ALL");
 
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
-	bool bAreColliding = false;
+	
 
 	//Collision check goes here
-	m_pMeshMngr->Print("x:" + std::to_string( m_v3Center1.x ) + " ", RERED);
-	m_pMeshMngr->Print("y:" + std::to_string(m_v3Center1.y) + " ", RERED);
-	m_pMeshMngr->Print("z:" + std::to_string(m_v3Center1.z) + " ", RERED);
+	vector3 v3Temp = vector3(m_m4Steve * vector4( m_v3Center1, 1.0f));
+	vector3 v3Temp1 = vector3(m_m4Creeper * vector4(m_v3Center2, 1.0f));
+	bool bAreColliding = false;
+	if(glm::distance(v3Temp, v3Temp1) < m_fRadius1 + m_fRadius2)
+		bAreColliding = true;
+
+	m_m4Steve = m_pMeshMngr->GetModelMatrix("Steve") * glm::translate(m_v3Center1);
+	if(bAreColliding)
+		m_pMeshMngr->AddSphereToQueue(m_m4Steve * glm::scale(vector3(m_fRadius1 * 2.0f)), RERED, WIRE);
+	else
+		m_pMeshMngr->AddSphereToQueue(m_m4Steve * glm::scale(vector3(m_fRadius1 * 2.0f)), REGREEN, WIRE);
+
+	m_m4Creeper = m_pMeshMngr->GetModelMatrix("Creeper") * glm::translate(m_v3Center2);
+	if (bAreColliding)
+		m_pMeshMngr->AddSphereToQueue(m_m4Creeper * glm::scale(vector3(m_fRadius2 * 2.0f)), RERED, WIRE);
+	else
+		m_pMeshMngr->AddSphereToQueue(m_m4Creeper * glm::scale(vector3(m_fRadius2 * 2.0f)), REGREEN, WIRE);
+
+	m_pMeshMngr->Print("x:" + std::to_string(v3Temp.x ) + " ", RERED);
+	m_pMeshMngr->Print("y:" + std::to_string(v3Temp.y) + " ", RERED);
+	m_pMeshMngr->Print("z:" + std::to_string(v3Temp.z) + " ", RERED);
 	m_pMeshMngr->PrintLine("");
 
 	//print info into the console
@@ -156,22 +179,7 @@ void AppClass::Display(void)
 		break;
 	}
 	
-	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
-	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
-
-	matrix4 m4Model =
-		m_pMeshMngr->GetModelMatrix("Steve") *
-		glm::translate(m_v3Center1) *
-		glm::scale(vector3(m_fRadius1 * 2.0f));
-	//m_pSphere1->Render(m4Projection, m4View, m4Model);
-	m_pMeshMngr->AddSphereToQueue(m4Model, RERED, WIRE);
-
-	m4Model =
-		m_pMeshMngr->GetModelMatrix("Creeper") *
-		glm::translate(m_v3Center2) *
-		glm::scale(vector3(m_fRadius2 * 2.0f));
-	//m_pSphere2->Render(m4Projection, m4View, m4Model);
-	m_pMeshMngr->AddSphereToQueue(m4Model, RERED, WIRE);
+	
 
 	m_pMeshMngr->Render(); //renders the render list
 
